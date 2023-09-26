49b1d777c08e2ab9ae8c181b4ac9bc25ce2d3763
Lee Hyohwak <crewe1746@naver.com>
2023-09-26 17:01:58 +0900

    Add stub connection handler to server

--- server/main.go
+++ server/main.go
@@ -3,7 +3,18 @@ package main
 import (
 	"log/slog"
 	"net"
+	"strings"
 )
+
+var helpMessage = `
+Commands:
+  create <room> - create a new room
+  join <room>   - join a room
+  nick <name>   - change nickname
+  list          - list all rooms
+  quit          - quit chat server
+  help          - show help
+`
 
 func main() {
 	listener, err := net.Listen("tcp", "127.0.0.1:8080")
@@ -13,6 +24,11 @@ func main() {
 
 	slog.Info("Chat server started on port 8080")
 
+	newConnections := make(chan net.Conn)
+	defer close(newConnections)
+
+	go handleConnections(newConnections)
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -20,8 +36,55 @@ func main() {
 			continue
 		}
 
-		go handleConnection(conn)
+		newConnections <- conn
 	}
 }
 
-func handleConnection(conn net.Conn) {}
+type User struct {
+	nickname string
+	net.Conn
+}
+
+func handleConnections(conns <-chan net.Conn) {
+	for conn := range conns {
+		user := User{nickname: "unknown", Conn: conn}
+
+		go func(user User) {
+			buf := make([]byte, 1024)
+
+			for {
+				_, err := user.Write([]byte("Enter command:"))
+				if err != nil {
+					slog.Error(err.Error())
+					return
+				}
+
+				n, err := user.Read(buf)
+				if err != nil {
+					slog.Error(err.Error())
+					return
+				}
+
+				msg := string(buf[:n])
+				fields := strings.Fields(msg)
+
+				switch fields[0] {
+				case "create":
+					// create room
+				case "join":
+					// join room
+				case "nick":
+					// change nickname
+				case "list":
+					// list rooms
+				case "quit":
+					// quit chat server
+				case "help":
+					// show help
+				default:
+					_, _ = user.Write([]byte("Unknown command\n"))
+				}
+			}
+		}(user)
+	}
+}
