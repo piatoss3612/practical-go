993d9c5ca79279ee3249e79aab1671847c35b325
Lee Hyohwak <crewe1746@naver.com>
2023-09-26 11:21:12 +0900

    Update listener and client to echoing messages

--- client/main.go
+++ client/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"io"
 	"log/slog"
 	"net"
 	"time"
 )
 
 func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			slog.Error("Panic recovered", slog.Any("panic", r))
+		}
+	}()
+
 	client, err := net.Dial("tcp", "127.0.0.1:8080")
 	if err != nil {
 		panic(err)
@@ -14,8 +21,26 @@ func main() {
 
 	slog.Info("TCP connection established", slog.String("address", client.LocalAddr().String()))
 
+	buf := make([]byte, 1024)
+
 	for i := 0; i < 10; i++ {
-		client.Write([]byte("Hello, world!"))
+		_, err := client.Write([]byte("Hello, world!"))
+		if err != nil {
+			slog.Error(err.Error())
+			return
+		}
+		slog.Info("Sent message", slog.String("message", "Hello, world!"))
+
+		n, err := client.Read(buf)
+		if err != nil {
+			if err != io.EOF {
+				slog.Error(err.Error())
+			}
+			return
+		}
+
+		slog.Info("Received message", slog.String("message", string(buf[:n])))
+
 		time.Sleep(time.Second)
 	}
 
--- listener/main.go
+++ listener/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"io"
 	"log/slog"
 	"net"
 )
 
 func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			slog.Error("Panic recovered", slog.Any("panic", r))
+		}
+	}()
+
 	listener, err := net.Listen("tcp", "127.0.0.1:8080")
 	if err != nil {
 		panic(err)
@@ -22,22 +29,37 @@ func main() {
 		for {
 			conn, err := listener.Accept()
 			if err != nil {
-				slog.Error(err.Error(), slog.String("from", "listener/main.go line:25"))
+				slog.Error(err.Error())
 				return
 			}
 
+			slog.Info("TCP connection established", slog.String("address", conn.RemoteAddr().String()))
+
 			go func(c net.Conn) {
-				defer c.Close()
+				defer func() {
+					_ = c.Close()
+					slog.Info("TCP connection closed", slog.String("address", conn.RemoteAddr().String()))
+				}()
 
 				buf := make([]byte, 1024)
 				for {
 					n, err := c.Read(buf)
 					if err != nil {
-						slog.Error(err.Error(), slog.String("from", "listener/main.go line:36"))
+						if err != io.EOF {
+							slog.Error(err.Error())
+						}
 						return
 					}
 
 					slog.Info("Received message", slog.String("message", string(buf[:n])))
+
+					_, err = c.Write(buf[:n])
+					if err != nil {
+						slog.Error(err.Error())
+						return
+					}
+
+					slog.Info("Echoed message", slog.String("message", string(buf[:n])))
 				}
 			}(conn)
 		}
