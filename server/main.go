fab21c912c57a3b79cc1ef764328c99ff89ade77
Lee Hyohwak <crewe1746@naver.com>
2023-09-26 15:50:01 +0900

    Add Dockerfile to build chat server as container image

--- server/main.go
+++ server/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"log/slog"
+	"net"
+)
 
+func main() {
+	listener, err := net.Listen("tcp", "127.0.0.1:8080")
+	if err != nil {
+		panic(err)
+	}
+
+	slog.Info("Chat server started on port 8080")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			slog.Error(err.Error())
+			continue
+		}
+
+		go handleConnection(conn)
+	}
 }
+
+func handleConnection(conn net.Conn) {}
