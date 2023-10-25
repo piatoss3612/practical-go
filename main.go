1507f9a2e5a8a1771d633db5ac5a6b87c6f03258
Lee Hyohwak <crewe1746@naver.com>
2023-10-25 21:13:51 +0900

    Add simple vault example

--- main.go
+++ main.go
@@ -1,3 +1,52 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"log"
+
+	vault "github.com/hashicorp/vault/api"
+)
+
+func main() {
+	config := vault.DefaultConfig()
+
+	config.Address = "http://127.0.0.1:8200"
+
+	client, err := vault.NewClient(config)
+	if err != nil {
+		log.Fatalf("Error creating vault client: %s", err)
+	}
+
+	client.SetToken("root")
+
+	secretData := map[string]interface{}{
+		"password": "Hashi123",
+	}
+
+	ctx := context.Background()
+
+	// Write a secret
+	_, err = client.KVv2("secret").Put(ctx, "my-secret-password", secretData)
+	if err != nil {
+		log.Fatalf("unable to write secret: %v", err)
+	}
+
+	log.Println("Secret written successfully.")
+
+	// Read a secret
+	secret, err := client.KVv2("secret").Get(ctx, "my-secret-password")
+	if err != nil {
+		log.Fatalf("unable to read secret: %v", err)
+	}
+
+	value, ok := secret.Data["password"].(string)
+	if !ok {
+		log.Fatalf("value type assertion failed: %T %#v", secret.Data["password"], secret.Data["password"])
+	}
+
+	if value != "Hashi123" {
+		log.Fatalf("unexpected password value %q retrieved from vault", value)
+	}
+
+	log.Println("Access granted!")
+}
