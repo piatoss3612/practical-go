8df7b79be83772cffba3e7710fc8568892b3a602
piatoss <crewe1746@naver.com>
2024-01-20 20:09:49 +0900

    Add docker-compose.yml

--- main.go
+++ main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"log"
 
 	_ "github.com/joho/godotenv/autoload"
-	"github.com/tmc/langchaingo/llms/openai"
-	"github.com/tmc/langchaingo/schema"
 )
 
 type Post struct {
@@ -27,77 +23,82 @@ type Summary struct {
 }
 
 func main() {
-	llm, err := openai.NewChat()
+	_, err := NewCache(context.Background(), "localhost:6379")
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	posts, done, errs := ScrapeTokenPost(true)
+	// 	llm, err := openai.NewChat()
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
 
-	stop := make(chan struct{})
-	summaries := make(chan *Summary)
+	// 	posts, done, errs := ScrapeTokenPost(true)
 
-	go func() {
-		defer close(stop)
+	// 	stop := make(chan struct{})
+	// 	summaries := make(chan *Summary)
 
-		for s := range summaries {
-			fmt.Println("========================================")
-			fmt.Println("요약된 내용:")
-			fmt.Println(s.Title)
-			fmt.Println(s.URL)
-			fmt.Println(s.Summary)
-			fmt.Println("========================================")
-		}
-	}()
+	// 	go func() {
+	// 		defer close(stop)
 
-	cnt := 0
-	ctx := context.Background()
+	// 		for s := range summaries {
+	// 			fmt.Println("========================================")
+	// 			fmt.Println("요약된 내용:")
+	// 			fmt.Println(s.Title)
+	// 			fmt.Println(s.URL)
+	// 			fmt.Println(s.Summary)
+	// 			fmt.Println("========================================")
+	// 		}
+	// 	}()
 
-scraper:
-	for {
-		select {
-		case post := <-posts:
-			if post == nil {
-				continue
-			}
+	// 	cnt := 0
+	// 	ctx := context.Background()
 
-			if cnt == 1 {
-				continue
-			}
+	// scraper:
+	// 	for {
+	// 		select {
+	// 		case post := <-posts:
+	// 			if post == nil {
+	// 				continue
+	// 			}
 
-			cnt++
+	// 			if cnt == 1 {
+	// 				continue
+	// 			}
 
-			content := fmt.Sprintf("제목: %s\n\n내용:\n%s", post.Title, post.Contents)
+	// 			cnt++
 
-			chat, err := llm.Call(ctx, []schema.ChatMessage{
-				schema.SystemChatMessage{
-					Content: "당신은 블록체인과 관련된 전문적인 지식을 갖추고 있습니다. 아래의 블록체인 관련 게시글의 본문에는 마크다운 형식으로 소제목이 포함되어 있습니다. 이를 요약해주세요.",
-				},
-				schema.HumanChatMessage{
-					Content: content,
-				},
-			})
-			if err != nil {
-				log.Fatal(err)
-			}
+	// 			content := fmt.Sprintf("제목: %s\n\n내용:\n%s", post.Title, post.Contents)
 
-			summaries <- &Summary{
-				Title:   post.Title,
-				URL:     post.URL,
-				Summary: chat.GetContent(),
-			}
-		case err := <-errs:
-			if err == nil {
-				continue
-			}
-			fmt.Println(err)
-		case <-done:
-			fmt.Println("Done")
-			break scraper
-		}
-	}
+	// 			chat, err := llm.Call(ctx, []schema.ChatMessage{
+	// 				schema.SystemChatMessage{
+	// 					Content: "당신은 블록체인과 관련된 전문적인 지식을 갖추고 있습니다. 아래의 블록체인 관련 게시글의 본문에는 마크다운 형식으로 소제목이 포함되어 있습니다. 이를 요약해주세요.",
+	// 				},
+	// 				schema.HumanChatMessage{
+	// 					Content: content,
+	// 				},
+	// 			})
+	// 			if err != nil {
+	// 				log.Fatal(err)
+	// 			}
 
-	close(summaries)
+	// 			summaries <- &Summary{
+	// 				Title:   post.Title,
+	// 				URL:     post.URL,
+	// 				Summary: chat.GetContent(),
+	// 			}
+	// 		case err := <-errs:
+	// 			if err == nil {
+	// 				continue
+	// 			}
+	// 			fmt.Println(err)
+	// 		case <-done:
+	// 			fmt.Println("Done")
+	// 			break scraper
+	// 		}
+	// 	}
 
-	<-stop
+	// 	close(summaries)
+
+	// <-stop
 }
