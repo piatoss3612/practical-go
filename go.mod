f96b92e5b98b9326537114fa21a2814346d7ac19
piatoss <crewe1746@naver.com>
2024-01-20 18:16:27 +0900

    Generate summary of the post using llm (openai)

--- go.mod
+++ go.mod
@@ -18,6 +18,7 @@ require (
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/pkoukk/tiktoken-go v0.1.2 // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/tmc/langchaingo/llms/openai"
+	"github.com/tmc/langchaingo/schema"
 )
 
 type Post struct {
@@ -10,16 +16,72 @@ type Post struct {
 	Contents string
 }
 
+type Summary struct {
+	Title   string
+	URL     string
+	Summary string
+}
+
 func main() {
+	llm, err := openai.NewChat()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	posts, done, errs := ScrapeTokenPost(true)
 
+	stop := make(chan struct{})
+	summaries := make(chan *Summary)
+
+	go func() {
+		defer close(stop)
+
+		for s := range summaries {
+			fmt.Println("========================================")
+			fmt.Println("요약된 내용:")
+			fmt.Println(s.Title)
+			fmt.Println(s.URL)
+			fmt.Println(s.Summary)
+			fmt.Println("========================================")
+		}
+	}()
+
+	cnt := 0
+	ctx := context.Background()
+
+scraper:
 	for {
 		select {
 		case post := <-posts:
 			if post == nil {
 				continue
 			}
-			fmt.Println(post)
+
+			if cnt == 1 {
+				continue
+			}
+
+			cnt++
+
+			content := fmt.Sprintf("제목: %s\n\n내용: %s", post.Title, post.Contents)
+
+			chat, err := llm.Call(ctx, []schema.ChatMessage{
+				schema.SystemChatMessage{
+					Content: "당신은 블록체인과 관련된 전문적인 지식을 갖추고 있습니다. 아래의 블록체인 관련 게시글을 읽고, 이를 10줄 이내로 요약해주세요.",
+				},
+				schema.HumanChatMessage{
+					Content: content,
+				},
+			})
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			summaries <- &Summary{
+				Title:   post.Title,
+				URL:     post.URL,
+				Summary: chat.GetContent(),
+			}
 		case err := <-errs:
 			if err == nil {
 				continue
@@ -27,7 +89,11 @@ func main() {
 			fmt.Println(err)
 		case <-done:
 			fmt.Println("Done")
-			return
+			break scraper
 		}
 	}
+
+	close(summaries)
+
+	<-stop
 }
